e9bcb4b0b956b6cf6a62562c58897148e7d1258f
Mani <mani.mozaffar@gmail.com>
2023-05-25 19:18:37 +0300

    added: protocols

--- poke_challenge/abstract.py
+++ poke_challenge/abstract.py
@@ -1,5 +1,5 @@
 from typing import List
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import httpx
 
@@ -7,26 +7,34 @@ from poke_challenge.schema import PokemonTypes, PokemonTypeCounter
 
 
 class APIHandlerABC(ABC):
+    @abstractmethod
     async def get_single_pokemon_data(
         client: httpx.AsyncClient, name: str
     ): ...
 
+    @abstractmethod
     def get_data_from_api(self) -> List[PokemonTypes]: ...
 
 
 class JsonFileHandlerABC(ABC):
-    def __init__(self, ): ...
+    @abstractmethod
     def pydantic_to_json(self, result: dict): ...
+    @abstractmethod
     def save_to_file(self, data: dict): ...
 
 
 class DatabaseHandlerABC(ABC):
-    def __init__(self): ...
+    @abstractmethod
     async def query_counter(self) -> List[PokemonTypeCounter]: ...
+    @abstractmethod
     async def start_database(self) -> None: ...
+    @abstractmethod
     async def close(self) -> None: ...
+    @abstractmethod
     async def store_result(self, result: List[PokemonTypes]) -> None: ...
+    @abstractmethod
     async def all_pokemons(self) -> List[PokemonTypes.dict]: ...
+    @abstractmethod
     async def tortoise_up(self) -> None: ...
 
 
@@ -35,11 +43,16 @@ class ControllerABC(ABC):
     josn_handler: JsonFileHandlerABC
     api_handler: APIHandlerABC
 
+    @abstractmethod
     async def store_all_results(self, result: List[PokemonTypes]) -> None: ...
 
+    @abstractmethod
     async def print_all_results_from_db(
         self, result: List[PokemonTypes]
     ) -> None: ...
+    @abstractmethod
     async def query_for_count(self, ) -> List[PokemonTypeCounter]: ...
+    @abstractmethod
     async def save_results_json(self) -> None: ...
+    @abstractmethod
     async def exit(self) -> None: ...
